96e31a36b1132726fa6e6314fb22bff0656c39c5
hs12 <77109954+hyun2371@users.noreply.github.com>
2022-03-01 23:07:12 +0900

    순환

---
+++ 자료구조/Fibonacci_Factorial.cpp
@@ -0,0 +1,16 @@
+/*#include <iostream>
+using namespace std;
+int fib(int n) {
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	return fib(n - 2) + fib(n - 1);
+}
+int fact(int n) {
+	if (n == 0) return 1;
+	if (n == 1) return 1;
+	return n*fact(n - 1);
+}
+int main() {
+	cout << fib(6) << endl;
+	cout << fact(6) << endl;
+}*/
